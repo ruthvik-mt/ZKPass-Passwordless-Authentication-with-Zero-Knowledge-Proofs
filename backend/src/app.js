a7956bb924604620afad7caaa7453c79f9d42037
Sanjay UG <122535741+SanjayUG@users.noreply.github.com>
2025-02-25 00:55:26 +0000

    init: backend setup.

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('ZKPass Backend is running...');
+});
+
+export { app };
